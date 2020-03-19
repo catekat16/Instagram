4ed17bdff2286cdc96a18033d30666033e410c08
Caitlyn Chen <cate.kat16@gmail.com>
2020-03-18 23:56:55 -0400

    commit for basic functioning Instagram

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Xiaoyan Yang on 3/18/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
